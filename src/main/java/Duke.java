11bda8c4d7401915bf29fb2852f6d0013cc22f37
CoolStatue <161408199+CoolStatue@users.noreply.github.com>
2026-08-15 07:30:04 +0800

    Add greeting and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,18 @@
 public class Duke {
     public static void main(String[] args) {
-        String banner = " ____        _        \n"
+        String BANNER = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println(banner);
+        String BAR = "____________________________________________________________";
+        String CHATBOT_NAME = "Flowey";
+        System.out.println(BAR);
+        System.out.println(BANNER);
+        System.out.println(String.format("Howdy! I'm %s!", CHATBOT_NAME));
+        System.out.println(BAR);
+        System.out.println("See ya!");
+        System.out.println(BAR);
+
     }
 }
