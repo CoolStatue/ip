64a644c538cf39bf469a65a4e35c18e8ca0e72b4
CoolStatue <161408199+CoolStatue@users.noreply.github.com>
2026-08-15 08:18:53 +0800

    Add list command functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -33,6 +34,7 @@ public class Duke {
         System.out.println(String.format("Howdy! I'm %s!", CHATBOT_NAME));
         System.out.println(BAR);
 
+        ArrayList<String> itemList = new ArrayList<String>();
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNextLine()) {
             String command = scanner.nextLine();
@@ -41,9 +43,19 @@ public class Duke {
                 System.out.println("\t" + "Bye. Hope to see you again soon!");
                 System.out.println("\t" + BAR);
                 break;
-            } else {
+            } else if (command.equals("list")) {
+                System.out.println(("\t" + BAR));
+                for (int i = 0; i < itemList.size(); i++) {
+                    System.out.print("\t");
+                    System.out.print(i + 1);
+                    System.out.println(". " + itemList.get(i));
+                }
+                System.out.println(("\t" + BAR));
+
+            }else {
+                itemList.add(command);
                 System.out.println("\t" + BAR);
-                System.out.println("\t" + command);
+                System.out.println("\t" + "added: " + command);
                 System.out.println("\t" + BAR);
             }
 
