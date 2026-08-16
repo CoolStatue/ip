b66ca9de4815fcb836f490dc13240b9435ff15cc
CoolStatue <161408199+CoolStatue@users.noreply.github.com>
2026-08-16 08:04:19 +0800

    Add mark and unmark commands

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -34,10 +34,11 @@ public class Duke {
         System.out.println(String.format("Howdy! I'm %s!", CHATBOT_NAME));
         System.out.println(BAR);
 
-        ArrayList<String> itemList = new ArrayList<String>();
+        ArrayList<Task> taskList = new ArrayList<Task>();
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNextLine()) {
             String command = scanner.nextLine();
+            String[] splitCommand = command.trim().split("\\s+", 2);
             if (command.equals("bye")) {
                 System.out.println("\t" + BAR);
                 System.out.println("\t" + "Bye. Hope to see you again soon!");
@@ -45,15 +46,29 @@ public class Duke {
                 break;
             } else if (command.equals("list")) {
                 System.out.println(("\t" + BAR));
-                for (int i = 0; i < itemList.size(); i++) {
+                for (int i = 0; i < taskList.size(); i++) {
                     System.out.print("\t");
                     System.out.print(i + 1);
-                    System.out.println(". " + itemList.get(i));
+                    System.out.println(". " + taskList.get(i).toString());
                 }
                 System.out.println(("\t" + BAR));
 
+            }else if (splitCommand[0].equals("mark") && splitCommand.length == 2 && splitCommand[1].chars().allMatch(Character::isDigit)) {
+                int index = Integer.parseInt(splitCommand[1]) - 1;
+                taskList.get(index).markAsComplete();
+                System.out.println("\t" + BAR);
+                System.out.println("\t" + "Nice! I've marked this task as done:");
+                System.out.println("\t\t" + taskList.get(index).toString());
+                System.out.println("\t" + BAR);
+            }else if (splitCommand[0].equals("unmark") && splitCommand.length == 2 && splitCommand[1].chars().allMatch(Character::isDigit)) {
+                int index = Integer.parseInt(splitCommand[1]) - 1;
+                taskList.get(index).markAsIncomplete();
+                System.out.println("\t" + BAR);
+                System.out.println("\t" + "OK, I've marked this task as not done yet:");
+                System.out.println("\t\t" + taskList.get(index).toString());
+                System.out.println("\t" + BAR);
             }else {
-                itemList.add(command);
+                taskList.add(new Task(command));
                 System.out.println("\t" + BAR);
                 System.out.println("\t" + "added: " + command);
                 System.out.println("\t" + BAR);
